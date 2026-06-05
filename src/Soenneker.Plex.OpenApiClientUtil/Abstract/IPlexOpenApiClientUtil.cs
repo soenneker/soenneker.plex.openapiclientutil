c08c18e4235d9b8ca5df2dcf1e62b338f25dd0d9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:12 -0500

    xml commenting

--- src/Soenneker.Plex.OpenApiClientUtil/Abstract/IPlexOpenApiClientUtil.cs
+++ src/Soenneker.Plex.OpenApiClientUtil/Abstract/IPlexOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Plex.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IPlexOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<PlexOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
